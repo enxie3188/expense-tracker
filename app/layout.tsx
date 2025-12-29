3f0d3a84b56e8b06f1e9cc13dabd5943c1f779b6
enxie3188 <mine921017@gmail.com>
2025-12-29 22:47:40 +0800

    Fix mobile safe area issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <meta name="description" content="Master Your Edge with AlphaLog" />
 
         {/* PWA Meta Tags */}
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
         <meta name="theme-color" content="#0B1120" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
